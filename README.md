7984f389b93e61cae6436a2fb3955d7e29d13be7
Pramodya De Alwis <pramodya1@hotmail.com>
2018-11-04 01:22:04 +1300

    removed unneeded files and added quick start script

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Dotfiles!
 
+## Quick start
+Running bootstrap.sh should link most of the files. Currently it does:
+    - .vimrc
+    - .zshrc
+    - i3 config
+    - nvim config
+
 ## Usage
 Need to link the file to the correct place on the disk i.e. `ln -s ~/dotfiles/.vimrc ~/`.
 Ideal to fork and keep your own version of the git or branch off. Pull requests welcome...there's a long way to go for this.
@@ -8,4 +15,4 @@ Ideal to fork and keep your own version of the git or branch off. Pull requests 
 Never link whole folders. Instead link the files inside the folders.
 
 ## TODO
-Quick start shell script for linux distros
+Add more links to the bootstrap.sh script
---
+++ bootstrap.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+git pull origin master;
+
+# Declare an associative array
+declare -A files
+
+# Define the files. The key is the file and the value is the destination.
+files=( \
+    [".vimrc"]="$HOME" \
+    [".zshrc"]="$HOME" \
+    [".tmux.conf"]="$HOME" \
+    ["i3/config"]="$HOME/.config/i3" \
+    ["nvim/init.vim"]="$HOME/.config/nvim" \
+)
+
+function linktocustom () {
+    ln -s $PWD/$1 $2 \
+}
+
+function run (){
+    for file in "${!files[@]}"
+        do linktocustom $file ${files[$file]}
+    done
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    run;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+        run
+	fi;
+fi;
+
+unset run;
